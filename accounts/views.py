849493eb74ab20bde8ad73c29515332ec9fce802
Vineet0105 <vineetpokharna7@gmail.com>
2026-02-13 12:14:32 +0530

    Add authentication logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from .serializer import *
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
 
-# Create your views here.
+class RegisterAPI(APIView):
+    def post(self, request):
+        data= request.data
+        serializer = RegisterSerializer(data=data)
+        if not serializer.is_valid():
+            data_json = {
+                "Status":True,
+                "message":serializer.errors
+            }
+            return Response(data_json,status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+        user = User.objects.get(username = data['username'])
+        token,_ = Token.objects.get_or_create(user=user)
+        data_json = {
+            "Status":True,
+            "Message": "Account Created",
+            "token" : str(token)
+            }
+
+        return Response(data_json,status.HTTP_201_CREATED)
+
+class LoginAPI(APIView):
+
+    def get(self,request):
+        data= request.data
+        serializer = LoginSerializer(data=data)
+        if not serializer.is_valid():
+            data_json = {
+                "Status":True,
+                "message":serializer.errors
+            }
+            return Response(data_json,status.HTTP_400_BAD_REQUEST)
+
+        username = data['username']
+        password = data['password']
+        user = authenticate(username=username,password=password)
+        if user==None:
+            data_json = {
+                "Status":True,
+                "message":serializer.errors
+            }             
+            return Response(data_json,status.HTTP_400_BAD_REQUEST)
+  
+        user = User.objects.get(username = data['username'])
+        token,_ = Token.objects.get_or_create(user=user)
+        data_json = {
+            "Status":True,
+            "Message": "Account Created",
+            "token" : str(token)
+            }
+        return Response(data_json,status.HTTP_200_OK)
+
--- todo/settings.py
+++ todo/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'home',
+    'accounts',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
