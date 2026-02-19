e0bb80e65ba94cec8e836e616814423bd5c5724f
Vineet0105 <vineetpokharna7@gmail.com>
2026-02-19 16:09:21 +0530

    Added Comments

--- home/models.py
+++ home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+#this is new changes
+
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     is_completed = models.BooleanField(default=False)
